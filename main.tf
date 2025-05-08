d231eecfee197f81e5df1e02a04ad98e6d6fa653
Dalairr <dalairfranzen@gmail.com>
2025-05-08 19:23:15 +0000

    version 1.0.0

--- main.tf
+++ main.tf
@@ -1,18 +1,32 @@
 terraform {
-  required_version = ">=1.3.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">3.43.0"
+      version = ">= 3.43.0"
     }
   }
 }
 
+# Local variable for tagging resources consistently
+locals {
+  tags = {
+    "Environment" = var.environment
+  }
+}
+
+# This module defines a secure Azure Storage Account
 resource "azurerm_storage_account" "securestorage" {
   resource_group_name           = var.resource_group_name
   location                      = var.location
   name                          = var.storage_account_name
-  account_tier                  = var.account_tier
-  account_replication_type      = var.account_replication_type
+  account_tier                  = "Standard"
+  
+  # Conditional logic: use GRS for Production, LRS for anything else
+  account_replication_type      = var.environment == "Production" ? "GRS" : "LRS"
+  
+  # Disable public access for security
   public_network_access_enabled = false
+
+  # Apply tags
+  tags = local.tags
 }
